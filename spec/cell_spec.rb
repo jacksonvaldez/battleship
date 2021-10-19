4d4f42dc12e8591a1f96461f2e8cc892e67e9247
Leland Curtis <lelandcurtis88@gmail.com>
2021-10-18 22:12:35 -0600

    Add render test

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -66,8 +66,37 @@ describe Cell do
   end
 
   describe ' #render'do
-    it 'returns correct display object for each condition' do
-
+    it 'returns "." if fired_upon == false and ship == nil' do
+      expect(@cell.render(false)).to eq (".")
+    end
+    it 'returns "." if fired_upon == false, ship == nil and render == true' do
+      expect(@cell.render(true)).to eq (".")
+    end
+    it 'returns "S" if fired_upon == false, ship == Ship and render == true' do
+      @cell.place_ship(@ship)
+      expect(@cell.render(true)).to eq ("S")
+    end
+    it 'returns "M" if fired_upon == true, ship == nil and ship != sunk' do
+      @cell.fire_upon
+      expect(@cell.render(true)).to eq ("M")
+      expect(@cell.render(false)).to eq ("M")
+    end
+    it 'returns "H" if fired_upon == true, ship == Ship and ship != sunk' do
+      @cell.place_ship(@ship)
+      @cell.fire_upon
+      expect(@cell.render(true)).to eq ("H")
+      expect(@cell.render(false)).to eq ("H")
+    end
+    it 'returns X if fired_upon == true and ship == Ship and ship == sunk'
+    @cell.place_ship(@ship)
+    # hit ship until health = 1
+    until @ship.health == 1
+      @ship.hit
+    end
+    @cell.fire_upon
+    expect(@cell.ship.sunk?).to eq (true)
+    expect(@cell.render(true)).to eq ("X")
+    expect(@cell.render(false)).to eq ("X")
     end
   end
 end
