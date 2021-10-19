9c8361af75f3fa7a13ba1af60fe55996bad15462
Leland Curtis <lelandcurtis88@gmail.com>
2021-10-18 18:45:58 -0600

    Add all test placeholders. Create attr tests. Create place_ship test. Complete empty? method.

--- lib/cell.rb
+++ lib/cell.rb
@@ -2,9 +2,9 @@ require './lib/ship'
 
 class Cell
   attr_reader :coordinate
-  attr_accessor :ship
+  attr_accessor :ship, :display
 
-  #initialize
+  #initialize cell with coordinate name and empty ship
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
@@ -16,8 +16,17 @@ class Cell
   end
 
   # method to check if cell is empty
-  def empty
+  def empty?
+    ship == nil
+  end
+
+  # method to check if cell has already been fired upon
+  def fired_upon?
 
   end
 
+  # method to return status of cell
+  def render
+
+  end
 end
---
+++ spec/cell_spec.rb
@@ -0,0 +1,55 @@
+require './lib/cell'
+require './lib/ship'
+
+describe Cell do
+  before(:each) do
+    @cell = Cell.new("B4")
+    @ship = Ship.new()
+  end
+
+  it 'is a cell' do
+    expect(@cell).to be_a Cell
+  end
+
+  it 'has correct coordinate' do
+    expect(@cell.coordinate).to eq("B4")
+  end
+
+  it 'initializes with nil ship' do
+    expect(@cell.ship).to eq(nil)
+  end
+
+  describe ' #place_ship' do
+    it 'adds ship object' do
+      @cell.place_ship(@ship)
+      expect(@cell.ship).to be_a Ship
+      expect(@cell.ship).to eq(@ship)
+    end
+
+
+  end
+
+  describe ' #empty?' do
+    it 'returns true when nil present' do
+
+    end
+    it 'returns false when ship present' do
+
+    end
+  end
+
+  describe ' #fired_upon' do
+    it 'returns false when display is "."' do
+
+    end
+    it 'returns true when display is anything but "."' do
+
+    end
+  end
+
+  describe ' #render'do
+    it 'returns correct display object for each condition' do
+
+    end
+  end
+end
