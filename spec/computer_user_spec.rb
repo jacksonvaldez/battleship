a0c3c71c9d4fc85962109ef6e04f6cb049dff369
Leland Curtis <lelandcurtis88@gmail.com>
2021-10-25 10:48:05 -0600

    Test: Add init and setup test.

--- spec/computer_user_spec.rb
+++ spec/computer_user_spec.rb
@@ -9,24 +9,38 @@ describe ComputerUser do
     @width = 4
     @height = 4
     @game = Game.new([@width, @height],@default_ships)
+    @steve = ComputerUser.new(Board.new([@width, @length]), @default_ships)
   end
 
-  # add init test
-  # add setup_board test
+  it 'exists' do
+    expect(@steve).to be_a(ComputerUser)
+  end
+  it 'has attributes' do
+    expect(@steve.ships).to eq(@default_ships)
+    expect(@steve.board).to be_a(Board)
+    expect(@steve.board.width).to eq(@width)
+    expect(@steve.board.height).to eq(@height)
+  end
 
+  describe ' #setup_board' do
+    it 'places all ships' do
+      @steve.setup_board
+      expect(@steve.board.cells.values.find_all{|cell| cell.empty?}.length).to eq(11)
+    end
+  end
 
   describe ' #create_cell_array' do
     it 'creates coordinates in up direction' do
-      expect(@game.computer_user.create_cell_array("C3", 3, "up")).to eq(["C3", "B3", "A3"])
+      expect(@steve.computer_user.create_cell_array("C3", 3, "up")).to eq(["C3", "B3", "A3"])
     end
     it 'creates coordinates in down direction' do
-      expect(@game.computer_user.create_cell_array("C3", 3, "down")).to eq(["C3", "D3", "E3"])
+      expect(@steve.computer_user.create_cell_array("C3", 3, "down")).to eq(["C3", "D3", "E3"])
     end
     it 'creates coordinates in left direction' do
-      expect(@game.computer_user.create_cell_array("C3", 3, "left")).to eq(["C3", "C2", "C1"])
+      expect(@steve.computer_user.create_cell_array("C3", 3, "left")).to eq(["C3", "C2", "C1"])
     end
     it 'creates coordinates in right direction' do
-      expect(@game.computer_user.create_cell_array("C3", 3, "right")).to eq(["C3", "C4", "C5"])
+      expect(@steve.computer_user.create_cell_array("C3", 3, "right")).to eq(["C3", "C4", "C5"])
     end
   end
 
