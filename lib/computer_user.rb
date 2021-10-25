7cc7fa8bd2334189f0189e07e72eb0dbf2cdc988
Leland Curtis <lelandcurtis88@gmail.com>
2021-10-25 11:35:17 -0600

    Create random_placement method to be called by setup_board. Update tests. Edge case still broken.

--- lib/computer_user.rb
+++ lib/computer_user.rb
@@ -1,3 +1,6 @@
+require './lib/board'
+require 'colorize' # gem install colorize in terminal if needed
+
 class ComputerUser
 
   attr_reader :board, :ships
@@ -11,36 +14,54 @@ class ComputerUser
 
 
   def setup_board
+    failed = false
     @ships.each do |ship|
-      # randomly choose starting cell.
-      # Generate 4 possible cell arrays based on ship length
-      # Check validity. Randomly choose one that works.
-      # Keep looping until valid placement found
-
-      chosen_placement = nil
-      possible_placements = []
-      valid_placements = []
-      while chosen_placement == nil do
-        if !@board.fits?(ship.length)
-          break
-        end
-        start_cell = @board.cells.values.sample
-        possible_placements << self.create_cell_array(start_cell.coordinate, ship.length, "up")
-        possible_placements << self.create_cell_array(start_cell.coordinate, ship.length, "down")
-        possible_placements << self.create_cell_array(start_cell.coordinate, ship.length, "left")
-        possible_placements << self.create_cell_array(start_cell.coordinate, ship.length, "right")
-        valid_placements = possible_placements.find_all{|placement| @board.valid_placement?(ship, placement)}
-        chosen_placement = valid_placements.sample
-      end
+      # randomly choose placement
+      chosen_placement = random_placement(ship)
       #place ship
       if chosen_placement.class == Array
         @board.place(ship,chosen_placement)
+      else
+        failed = true
+      end
+    end
+    if failed
+      # return failure error and make human the winner
+      puts "Steve failed to setup his board properly. You Win!!"
+      puts "To make it easier for Steve, try again with a larger board or fewer ships."
+      # destroy all computer ships to trigger end of game
+      @ships.each do |ship|
+        while ship.health > 0 do
+          ship.hit
+        end
       end
     end
   end
 
-
-
+  def random_placement(ship)
+    # randomly choose starting cell.
+    # Generate 4 possible cell arrays based on ship length
+    # Check validity. Randomly choose one that works.
+    # Keep looping until valid placement found
+    chosen_placement = nil
+    possible_placements = []
+    valid_placements = []
+    # first, check that ship can fit.
+    # If not, reset
+    while chosen_placement == nil do
+      if !@board.fits?(ship.length)
+        return nil
+      end
+      start_cell = @board.cells.values.sample
+      possible_placements << self.create_cell_array(start_cell.coordinate, ship.length, "up")
+      possible_placements << self.create_cell_array(start_cell.coordinate, ship.length, "down")
+      possible_placements << self.create_cell_array(start_cell.coordinate, ship.length, "left")
+      possible_placements << self.create_cell_array(start_cell.coordinate, ship.length, "right")
+      valid_placements = possible_placements.find_all{|placement| @board.valid_placement?(ship, placement)}
+      chosen_placement = valid_placements.sample
+    end
+    chosen_placement
+  end
 
   def create_cell_array(coordinate, length, direction)
     alphabet = ('A'..'Z').to_a
--- spec/computer_user_spec.rb
+++ spec/computer_user_spec.rb
@@ -8,8 +8,9 @@ describe ComputerUser do
     @default_ships = [Ship.new("Cruiser", 3), Ship.new("Submarine", 2)]
     @width = 4
     @height = 4
-    @game = Game.new([@width, @height],@default_ships)
-    @steve = ComputerUser.new(Board.new([@width, @length]), @default_ships)
+    dimensions = [@width, @length]
+    #@game = Game.new([@width, @height],@default_ships)
+    @steve = ComputerUser.new(Board.new(@height, @width), @default_ships)
   end
 
   it 'exists' do
@@ -25,22 +26,27 @@ describe ComputerUser do
   describe ' #setup_board' do
     it 'places all ships' do
       @steve.setup_board
-      expect(@steve.board.cells.values.find_all{|cell| cell.empty?}.length).to eq(11)
+      expect(@steve.board.cells.values.find_all{|cell| cell.empty?}.length).to eq(16 - 5)
+    end
+    xit 'returns all sunken ships if board cannot be set up' do
+      @bad_steve = ComputerUser.new(Board.new(2, 2), @default_ships)
+      @bad_steve.setup_board
+      expect(@bad_steve.ships.find_all{|ship| ship.sunk?}.length).to eq (2)
     end
   end
 
   describe ' #create_cell_array' do
     it 'creates coordinates in up direction' do
-      expect(@steve.computer_user.create_cell_array("C3", 3, "up")).to eq(["C3", "B3", "A3"])
+      expect(@steve.create_cell_array("C3", 3, "up")).to eq(["C3", "B3", "A3"])
     end
     it 'creates coordinates in down direction' do
-      expect(@steve.computer_user.create_cell_array("C3", 3, "down")).to eq(["C3", "D3", "E3"])
+      expect(@steve.create_cell_array("C3", 3, "down")).to eq(["C3", "D3", "E3"])
     end
     it 'creates coordinates in left direction' do
-      expect(@steve.computer_user.create_cell_array("C3", 3, "left")).to eq(["C3", "C2", "C1"])
+      expect(@steve.create_cell_array("C3", 3, "left")).to eq(["C3", "C2", "C1"])
     end
     it 'creates coordinates in right direction' do
-      expect(@steve.computer_user.create_cell_array("C3", 3, "right")).to eq(["C3", "C4", "C5"])
+      expect(@steve.create_cell_array("C3", 3, "right")).to eq(["C3", "C4", "C5"])
     end
   end
 
